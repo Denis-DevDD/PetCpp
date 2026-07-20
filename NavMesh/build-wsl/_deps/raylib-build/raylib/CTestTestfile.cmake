955c2c6b6b9f6ef5901b3743a015ef93114c70b0
Denis Poloiko <denis.poloiko@student.tugraz.at>
2026-07-20 12:04:01 +0200

    removed wsl build

--- NavMesh/build-wsl/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/denis/Desktop/PetCpp/NavMesh/build-wsl/_deps/raylib-src/src
-# Build directory: /mnt/c/Users/denis/Desktop/PetCpp/NavMesh/build-wsl/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
